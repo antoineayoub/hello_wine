a61e724fa689e448fa51f5fcc2e7435d36fd039b
Adrien de Villoutreys <adrien.de.villoutreys@gmail.com>
2016-03-02 15:11:01 +0100

    adv post react & controller ok > bi needed

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,16 +6,18 @@ class WinesController < ApplicationController
     @longitude = params[:longitude]
     @skip_navbard = true
 
-    @wines = Wine.all.first(5)
+    @wines = Wine.all
 
-    # @wines = @wines.filter_by_location(@latitude, @longitude)
-    # @wines = @wines.filter_by_color(params[:color])
-    # @wines = @wines.filter_by_price(params[:price])
+    @wines = @wines.filter_by_location(@latitude, @longitude)
 
-    #@wines = @wines.filter_by_pairing(params[:pairing])
+    @wines = @wines.filter_by_color(params[:color])
 
-    @wines = wine_sorting(@wines, 48.87146, 2.35500)
-    @wines = @wines.first(5)
+    @wines = @wines.filter_by_price(params[:price])
+
+   #@wines = @wines.filter_by_pairing(params[:pairing])
+
+    @wines = wine_sorting(@wines, @latitude, @longitude)
+
   end
 
   def show
@@ -44,25 +46,22 @@ class WinesController < ApplicationController
     wines.each do |wine|
       info_store = wine.nearest(latitude,longitude)
 
+
       rating_value = wine.external_ratings[2].avg_rating unless wine.external_ratings[2].nil?
       rating_score = [0.3*(rating_value - 2)*100,0].max unless rating_value.nil?
 
-      #distance_value = ( info_store[:distance] * 1000 ).round
-      #distance_score = 100 - distance_value / 6
+      distance_value = ( info_store[:distance] * 1000 ).round
+      distance_score = 100 - distance_value / 6
 
       if rating_score.nil?
         score = 0
       else
-        score = (rating_score)/2
+        score = (rating_score + distance_score)/2
       end
-      wines_matrix << { wine: wine, score: 80, info_store: {distance: 100} }
-
+      wines_matrix << { wine: wine, score: score, info_store: info_store }
     end
 
     @wines = wines_matrix.sort { | a, b | b[:score] <=> a[:score] }
   end
 
 end
-
-
-
