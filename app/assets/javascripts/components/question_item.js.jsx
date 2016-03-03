78179c8c58fd6095f953e7aaa1c4c072d476b7ae
Adrien de Villoutreys <adrien.de.villoutreys@gmail.com>
2016-03-03 16:31:25 +0100

    adrien

--- app/assets/javascripts/components/question_item.js.jsx
+++ app/assets/javascripts/components/question_item.js.jsx
@@ -1,4 +1,9 @@
 var QuestionItem = React.createClass({
+  getInitialState: function(){
+    return {
+      clicked: false
+    }
+  },
 
   handleClick: function(){
     if (this.props.isLastQuestion === true) {
@@ -11,14 +16,23 @@ var QuestionItem = React.createClass({
       this.props.onQuestionClick(this.props.value);
       this.props.onQuestionClicks(this.props.value);
     };
+    this.setState({
+      clicked: true
+    })
   },
 
-  render: function() {
+  // kframe 0% transform rotate 0
+  // animation le nom de ma keyframe .3 infinite
 
+  render: function() {
+    var btnClasses = classNames({
+      // 'is-clicked': this.state.clicked,
+      'btn btn-question': true
+    })
     return (
       <div className="col-xs-12">
         <div className="padded-top-bottom-xs">
-          <a className="btn btn-question" onClick={this.handleClick}>
+          <a className={btnClasses} onClick={this.handleClick}>
             {this.props.answer}
           </a>
         </div>
