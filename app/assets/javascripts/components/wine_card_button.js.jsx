a2bf884f82b09d5556fa213bde02cbb2d6dc4c62
antoine ayoub <antoine.ayoub@gmail.com>
2016-03-04 16:31:51 +0100

    add onTouch

--- app/assets/javascripts/components/wine_card_button.js.jsx
+++ app/assets/javascripts/components/wine_card_button.js.jsx
@@ -1,14 +1,14 @@
 var WineCardButton = React.createClass({
 
   // add function to handleName with if condition
-  handleClick: function(){
+  handleClick: function() {
     document.location.href = "/wines/"+this.props.wine.id+"?latitude="+this.props.latitude+"&longitude="+this.props.longitude+"&store_id="+this.props.wine.store.id+"/"
   },
   render: function() {
     return (
       <div>
         <div className="card-title">
-          <a onClick={this.handleClick} className="button-submit">GO GET IT</a>
+          <a onClick={this.handleClick} onTouch={this.handleClick} className="button-submit">GO GET IT</a>
         </div>
       </div>
     );
