cd42cc9a6ae199591ae0b2b122193a7e604b1870
Adrien de Villoutreys <adrien.de.villoutreys@gmail.com>
2016-03-04 12:35:00 +0100

    adrien

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,17 @@
 //= require pubsubjs
 //= require components
 //= require_tree .
+
+
+// create a function to subscribe to topics
+// var mySubscriber = function( msg, data ){
+//     console.log( msg, data );
+// };
+
+// add the function to the list of subscribers for a particular topic
+// we're keeping the returned token, in order to be able to unsubscribe
+// from the topic later on
+// var token = PubSub.subscribe( 'MY TOPIC', mySubscriber );
+
+// publish a topic asyncronously
+// PubSub.publish( 'MY TOPIC', 'hello world!' );
