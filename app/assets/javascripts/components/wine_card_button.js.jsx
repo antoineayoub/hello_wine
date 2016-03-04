aca55f5333a553518649ae33d916b93aada9acbd
antoine ayoub <antoine.ayoub@gmail.com>
2016-03-04 16:42:18 +0100

    bug fixing

--- app/assets/javascripts/components/wine_card_button.js.jsx
+++ app/assets/javascripts/components/wine_card_button.js.jsx
@@ -2,13 +2,13 @@ var WineCardButton = React.createClass({
 
   // add function to handleName with if condition
   handleClick: function() {
-    document.location.href = "/wines/"+this.props.wine.id+"?latitude="+this.props.latitude+"&longitude="+this.props.longitude+"&store_id="+this.props.wine.store.id+"/"
+    window.location.href = "/wines/"+this.props.wine.id+"?latitude="+this.props.latitude+"&longitude="+this.props.longitude+"&store_id="+this.props.wine.store.id+"/"
   },
   render: function() {
     return (
       <div>
         <div className="card-title">
-          <div onClick={this.handleClick} onTouch={this.handleClick} className="button-submit">GO GET IT</div>
+          <div onClick={this.handleClick} onTouch={this.handleClick} onPress={this.handleClick} onClick={this.handleClick} className="button-submit">GO GET IT</div>
         </div>
       </div>
     );
